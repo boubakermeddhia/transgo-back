15e0489076fe54f55b55c68f78de4575b5a30c81
unknown <mohameddhiaa.boubaker@polytechnicien.tn>
2022-02-26 23:45:51 +0100

    update

--- routes/admin/client.js
+++ routes/admin/client.js
@@ -35,7 +35,7 @@ router.route('/getordre/:id').post(Auth, async (req, res) => {
         const user = await User.findById(req.userid)
         if (user.secure == "2af264b99ff1d93e9477482ed9037db8") {
             const result = await Ordre.find({ idclient: req.params.id })
-            res.json({ result, status: 200 })
+            res.json({ result: result, status: 200 })
         } else {
             res.json({ status: 400 })
         }
