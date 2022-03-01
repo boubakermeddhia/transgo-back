316a231b1060e649708a9574454f8281d8ccc506
unknown <mohameddhiaa.boubaker@polytechnicien.tn>
2022-03-01 14:37:39 +0100

    update

--- routes/ordre/documents/bondesortie.js
+++ routes/ordre/documents/bondesortie.js
@@ -16,7 +16,11 @@ module.exports = ({ user, l }) => {
   }
 
   for (let i = 0; i < l.length; i++) {
-    somme = Number(l[i].prix) * Number(l[i].qte)
+    if (l[i].frais_inclus) {
+      somme = somme + Number(l[i].prix) * Number(l[i].qte) + Number(l[i].frais_sup)
+    } else {
+      somme = somme + Number(l[i].prix) * Number(l[i].qte) + Number(l[i].frais_sup) + Number(l[i].frais_colis)
+    }
     let uricodeabaree = "https://barcode.tec-it.com/barcode.ashx?data=" + `${l[i]._id}` + "&code=Code128&translate-esc=on"
     text = text + "<tr>" + "<th>" + String(i + 1) + "</th>" + "<th><br>" + `<img src=${uricodeabaree} style="width:80%;">` + "</th>" + "<th>" + l[i].name + "</th>" + "<th>" + l[i].adresse + "</th>" + "<th>" + l[i].numerotel + "</th>" + "<th>" + l[i].naturecolis + "</th>" + "</th>" + "<th>" + String(somme) + "</th>" + "</tr>"
     somme = 0
