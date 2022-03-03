387c70bde2aa75f309e4a0f8176badfaae07ed6b
unknown <mohameddhiaa.boubaker@polytechnicien.tn>
2022-03-03 12:55:30 +0100

    update

--- routes/client/signclient.js
+++ routes/client/signclient.js
@@ -82,7 +82,6 @@ router.route('/payment').post(Auth, async (req, res) => {
         console.log(use)
         if (admin.secure == "2af264b99ff1d93e9477482ed9037db8") {
             const existinguser = await User.findById(use)
-            console.log(existinguser)
 
             if (existinguser.secure == "577d7068826de925ea2aec01dbadf5e4") {
                 if (existinguser) {
