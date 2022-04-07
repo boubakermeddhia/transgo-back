ddf708209bd9cf5d917a27029fcee04e3e1f224b
unknown <mohameddhiaa.boubaker@polytechnicien.tn>
2022-04-07 22:14:39 +0100

    update

--- mailer.js
+++ mailer.js
@@ -2,6 +2,7 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (email, subject, text) => {
     try {
+        
         const transporter = nodemailer.createTransport({
         service: 'gmail',
             auth: {
